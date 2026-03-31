71de318aa97efbd3ecee5225baa4d9916e1ee1c7
languard <languard@gmail.com>
2026-03-31 11:22:32 -0400

    Initial Commit

---
+++ Assignment/main.cpp
@@ -0,0 +1,31 @@
+/*
+    Assignment: Rolling Dice with Classes
+    Goal: Create a basic class that will return either single rolls or an array of dice rolls
+
+    Requirements:
+    * No generation of random numbers is allowed in main.cpp
+    * You must have class.cpp and class.h files in your project
+    * (not literally called class, the file name/class name can be what you want)
+    * Your class must have a function for rolling and returning a single die
+    * Your class must have a function that takes a single int parameter for number of dice to roll,
+    *      and returns an array of ints containing the dice rolls
+    * The class must have a private variable that defines how many faces the dice have
+    * You must not have a function to change the dice faces. Each type of dice will need it's own declaration
+    * In main.cpp you will:
+    * * Declare three instances of your class, one for 4 sided dice, one for 8 sided dice, and one for 20 sided dice
+    * * You will roll a single die of each and display the value
+    * * You will then have a loop asking:
+    * * * What dice type to roll, with -1 exiting. If the input is not 4, 8, or 20 then repeat the question.
+    * * * How many dice to roll. If the value is less than 1, display a message and set the dice count to 1.
+    * * * Display all the dice rolls in a single line. Wrap-around does not count against this.
+    * Sample output is in the sampleoutput.txt file.
+    * You will loose 2 points if your code leaks memory.
+*/
+
+#include <iostream>
+
+
+int main()
+{    
+
+}
